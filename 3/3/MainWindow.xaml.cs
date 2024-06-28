947c99a592e52575352aaa6d82e033c9c5b8c4c0
Ibnodam <144240270+Ibnodam@users.noreply.github.com>
2024-06-28 13:29:53 +0200

    Add files via upload

---
+++ 3/3/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+       
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                ModeWindow window = new ModeWindow(tb_header.Text,
+
+                    Convert.ToInt32(tb_coor_x.Text), Convert.ToInt32(tb_coor_y.Text), Convert.ToInt32(tb_horizont.Text),
+                    Convert.ToInt32(tb_vertical.Text), tb_color.Text,Convert.ToBoolean(cb_isvisible.IsChecked));
+                    if (!Convert.ToBoolean(cb_isvisible.IsChecked)) window.Hide();
+
+
+
+
+
+            }
+
+            catch (Exception ex) { MessageBox.Show($"Введен неверный формат поля/полей. Подробнее:  {ex}"); }
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ 3/3/ModeWindow.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _3
+{
+
+
+    public class ModeWindow : Window
+    {
+        private string? header;
+        private int[] cordinates = new int[2];
+        private int horizontal_length;
+        private int vertical_length;
+        private string color;
+        private bool isvisible;
+        private bool borderOn;
+
+        public string? Header { get { return header; } set { if (value != "") header = value; else header = "_Default_"; } }
+
+        public int[] Cordinates
+        {
+            get { return cordinates; }
+            set
+            {
+                cordinates[0] = Convert.ToInt32(value[0]);
+                cordinates[1] = Convert.ToInt32(value[1]);
+            }
+        }
+
+
+        public int Horizontal_length { get { return horizontal_length; } set { if (value >= 1) horizontal_length = value; else horizontal_length = 1; } }
+
+        public int Vertical_length { get { return vertical_length; } set { if (value >= 1) vertical_length = value; else vertical_length = 1; } }
+
+        public string Color { get { return color; } set { if (value != "") color = value; else color = "Honeydew"; } }
+
+        public bool Isvisible { get { return isvisible; } set { isvisible = value; } }
+        public bool BorderOn { get { return borderOn; } set { borderOn = value; } }
+
+
+        public ModeWindow(string header, int coor_x, int coor_y, int horizont, int vertical, string color, bool isvisible)
+        {
+            Header = header;
+            Cordinates[0] = coor_x;
+            Cordinates[1] = coor_y;
+            Horizontal_length = horizont;
+            Vertical_length = vertical;
+            Color = color;
+            this.Title = Header;
+            this.Top = Cordinates[0];
+            this.Left = Cordinates[1];
+            this.Width = Horizontal_length;
+            this.Height = Vertical_length;
+            
+            this.Show();
+
+
+        }
+
+
+    }
+
+}
